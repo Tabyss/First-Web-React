37a7e1c6b440caf346d1f47874bc54fa006aa01c
toby <shabbahatha86@gmail.com>
2022-10-01 12:03:09 +0700

    First Step

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import logo from "./THABYS.png";
+import "./App.css";
+
+export default function Navbar() {
+  return (
+    <nav className="nav-1">
+      <img src={logo} height="30px" />
+      <div className="menu">
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Masuk</a>
+      </div>
+    </nav>
+  );
+}
