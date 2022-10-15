8eb31879f59b4369b98801ece408e00064edb2e6
toby <shabbahatha86@gmail.com>
2022-10-15 21:08:11 +0700

    Update about

--- src/component/about.js
+++ src/component/about.js
@@ -1,19 +1,26 @@
 import "../assets/css/about.css";
 import profile from "../assets/img/profile.png";
-
+import play from "../assets/img/play.svg";
 
 function About() {
   return (
     <div className="about">
-        <h1 className="about-value">about</h1>
-        <img src={ profile } />
-        <div className="about-data">
+      <h1 className="about-value">about</h1>
+      <div className="about-1">
+        {/* <img src={profile}  className="profile"/> */}
+        <div className="about-2">
           <p className="desc">about aku .....</p>
-          <h4 className="class-1">graphic designer</h4>
-          <h4 className="class-2">frontend development</h4>
+          <div className="about-3">
+            <img src={play} className="icon-about" />
+            <div className="about-4">
+              <h4 className="pos-1">graphic designer</h4>
+              <h4 className="pos-2">frontend development</h4>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About;
