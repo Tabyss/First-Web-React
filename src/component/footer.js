96174d8647e4300ac47568531bae981b18687539
toby <shabbahatha86@gmail.com>
2022-11-02 15:27:03 +0700

    update footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,32 +1,30 @@
 import "../assets/css/footer.css";
-import logo from "../assets/img/logo.svg";
-import React from 'react';
+import { ReactComponent as Logo } from "../assets/img/logo.svg";
+import React from "react";
 
 function Footer() {
   return (
     <div className="foot">
-        <div className="foot__copy">
-            <div className="foot__logo">
-                <img src={ logo } />
-                <h1>thabys</h1>
-            </div>
-            <p>copyright by thabys</p>
+      <div className="foot__copy">
+        <div className="foot__logo">
+          <Logo fill="white" width="110px" />
+          <h1>thabys</h1>
         </div>
-        <div>
-            <div>
-                <h3>Contact Me</h3>
-                <a>shabbahatha86@gmail.com</a>
-            </div>
-            <div>
-                <h3>Social</h3>
-                <a>Instagram</a>
-                <a>Behance</a>
-                <a>Twitter</a>
-                <a>LinkIn</a>
-            </div>
-        </div>
+        <i>© 2022 thabys</i>
+      </div>
+      <div className="foot__contact">
+        <h3>Contact Me</h3>
+        <a>shabbahatha86@gmail.com</a>
+      </div>
+      <div className="foot__social">
+        <h3>Social</h3>
+        <a>Instagram</a>
+        <a>Behance</a>
+        <a>Twitter</a>
+        <a>LinkIn</a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
