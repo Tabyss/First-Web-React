d3728eb8ea78e584aabe924bb743973cf9e98fc8
toby <shabbahatha86@gmail.com>
2022-11-02 15:26:47 +0700

    update navbar

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
-import logo from "../assets/img/logo.svg";
+import { ReactComponent as Logo } from '../assets/img/logo.svg';
 import "../assets/css/navbar.css";
 
-
 export default function Navbar() {
-
   const [click, setclick] = useState(false);
 
   const active = () => setclick(!click);
 
   return (
     <div className="nav">
-      <nav className="nav-1">
-        <img src={logo} height="50px" />
-        <div className="menu">
+      <nav className="nav__body">
+        <Logo height="35px" />
+        <div className="nav__menu">
+          <div className="burger" onClick={active}>
+            <button className={click ? "burger active" : "burger"}>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
           <a href="#">galleries</a>
           <a href="#">bio</a>
           <a href="#">skills</a>
           <a href="#">contact</a>
-        </div>
-        <div className="burger-1" onClick={active}>
-          <button className={click ? "burger active" : "burger"}>
-            <span></span>
-            <span></span>
-          </button>
         </div>
       </nav>
     </div>
